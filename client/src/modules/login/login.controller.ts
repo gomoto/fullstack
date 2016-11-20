2efcbdfb10767cd3af48faaff5825548b9367a25
Ryan Gomoto <ryan@cyrusbio.com>
2016-11-20 14:58:42 -0800

    Create basic login form

--- client/src/modules/login/login.controller.ts
+++ client/src/modules/login/login.controller.ts
@@ -1,13 +1,36 @@
-import { ILogService } from 'angular';
+import * as angular from 'angular';
 
 export default class LoginController {
 
   static $inject = [
+    '$auth',
     '$log'
   ];
 
   constructor(
-    private $log: ILogService
+    private $auth: angular.stormpath.IAuthService,
+    private $log: angular.ILogService
   ) {}
+
+  username: string;
+  password: string;
+
+  posting = false;
+  error: string;
+
+  submit(): void {
+    this.posting = true;
+    this.error = null;
+    this.$auth.authenticate({
+      username: this.username,
+      password: this.password
+    })
+    .catch((err) => {
+      this.error = err.message;
+    })
+    .finally(() => {
+      this.posting = false;
+    });
+  }
 
 }
