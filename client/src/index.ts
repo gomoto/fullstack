f465af403cde9cda50934a266f1a00cd8f073d0e
Ryan Gomoto <ryan@cyrusbio.com>
2016-11-19 17:01:25 -0800

    Include stormpath-sdk-angularjs in client

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,10 +1,13 @@
 import angular = require('angular');
 import 'angular-ui-router';
+import stormpath = require('stormpath-sdk-angularjs');
 import routes from './config/routes';
 import widget from './modules/widget/widget.module';
 
 angular.module('app', [
   'ui.router',
+  stormpath,
+  'stormpath.templates',
   widget
 ])
 .config(routes);
