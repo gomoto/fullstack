d0c5bd04668384f8f91c651c4e19ecdf7888a6c5
Ryan Gomoto <ryan@cyrusbio.com>
2016-12-16 18:50:04 -0800

    Remove unused login component

--- client/src/components/login/login.module.ts
+++
@@ -1,9 +0,0 @@
-import angular = require('angular');
-import LoginController from './login.controller';
-
-export default angular.module('components.login', [])
-.component('login', {
-  templateUrl: 'client/src/components/login/login.html',
-  controller: LoginController
-})
-.name;
