95a37ff8b4c579ed4a07ce51fe0e1f6ecf3408df
Ryan Gomoto <ryan@cyrusbio.com>
2016-12-19 18:00:36 -0800

    Parameterize logout form

--- client/src/components/dashboard/dashboard.controller.ts
+++ client/src/components/dashboard/dashboard.controller.ts
@@ -2,6 +2,20 @@ import * as jsCookie from 'js-cookie';
 
 export default class DashboardController {
 
-  private xsrf = jsCookie.get('XSRF-TOKEN');
+  static $inject = [
+    'STORMPATH_CONFIG'
+  ];
+
+  private logoutUri: string;
+  private xsrfName: string;
+  private xsrf: string;
+
+  constructor(
+    private STORMPATH_CONFIG: ng.stormpath.IStormpathConfig
+  ) {
+    this.logoutUri = STORMPATH_CONFIG.DESTROY_SESSION_ENDPOINT;
+    this.xsrfName = '_csrf';
+    this.xsrf = jsCookie.get('XSRF-TOKEN');
+  }
 
 }
