1760af50dc53beb588815e5d2324538f856111ea
Ryan Gomoto <ryan@cyrusbio.com>
2016-12-19 16:52:07 -0800

    Clean up

--- client/src/components/dashboard/dashboard.controller.ts
+++ client/src/components/dashboard/dashboard.controller.ts
@@ -1,21 +1,6 @@
-import { ILogService } from 'angular';
 import * as jsCookie from 'js-cookie';
 
 export default class DashboardController {
-
-  static $inject = [
-    '$auth',
-    '$http',
-    '$state',
-    'STORMPATH_CONFIG'
-  ];
-
-  constructor(
-    private $auth: ng.stormpath.IAuthService,
-    private $http: ng.IHttpService,
-    private $state: ng.ui.IStateService,
-    private STORMPATH_CONFIG: ng.stormpath.IStormpathConfig
-  ) {}
 
   private xsrf = jsCookie.get('XSRF-TOKEN');
 
