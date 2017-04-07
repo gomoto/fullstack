26a5b16454395922fdf359f6c55c0036ff2f1fd4
Ryan Gomoto <ryan@cyrusbio.com>
2017-04-06 20:09:25 -0700

    Create settings file

---
+++ server/src/config/settings.ts
@@ -0,0 +1,38 @@
+import path = require('path');
+
+const settings = {
+  env: process.env.NODE_ENV || 'development',
+
+  // Root path of server
+  root: path.normalize(`${__dirname}/../../..`),
+
+  // Server port
+  port: process.env.PORT || 9000,
+
+  // Server IP
+  ip: process.env.IP || '0.0.0.0',
+
+  login: '/login',
+
+  logout: '/logout',
+
+  apiGroups: process.env.API_GROUPS && process.env.API_GROUPS.split(',') || [],
+
+  adminGroups: process.env.ADMIN_GROUPS && process.env.ADMIN_GROUPS.split(',') || [],
+
+  cookieSecret: process.env.COOKIE_SECRET,
+
+  mongo: {
+    db: process.env.MONGO_DB || 'local',
+    host: process.env.MONGO_HOST || 'localhost',
+    port: process.env.MONGO_PORT || '27017'
+  },
+
+  // Should we populate the DB with sample data?
+  seedDB: false,
+
+  // List of user roles
+  userRoles: ['guest', 'user', 'admin']
+}
+
+export { settings }
