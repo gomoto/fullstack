4021b669d1d92d04131f5104705c9d7c75356d65
Ryan Gomoto <ryan@cyrusbio.com>
2017-04-18 17:24:16 -0700

    Check user permissions when getting or creating things

--- server/src/routes/api/thing/thing.controller.ts
+++ server/src/routes/api/thing/thing.controller.ts
@@ -11,6 +11,16 @@ import express = require('express');
 import * as mongodb from 'mongodb';
 import { ThingModel, ThingModelFactory } from './thing.model';
 
+function doesUserHavePermission(user: any, permission: string): boolean {
+  return (
+    user &&
+    user.app_metadata &&
+    user.app_metadata.authorization &&
+    user.app_metadata.authorization.permissions &&
+    user.app_metadata.authorization.permissions.indexOf(permission) !== -1
+  );
+}
+
 class ThingController {
   private model: ThingModel;
 
@@ -20,11 +30,10 @@ class ThingController {
 
   // Gets a list of Things
   public index(req: express.Request, res: express.Response) {
-    // console.log(req.user.app_metadata.authorization.permissions);
-    // if (req.user.app_metadata.authorization.permissions.indexOf('read:thing') === -1) {
-    //   this.respondWithResult(res, 401)({message: 'Not allowed!'});
-    //   return;
-    // }
+    if (!doesUserHavePermission(req.user, 'read:thing')) {
+      this.respondWithResult(res, 401)({message: 'Not allowed to read things!'});
+      return;
+    }
     const user_id = (req.user && req.user.user_id) || null;
     this.model.findAllForUser(user_id)
     .then(this.respondWithResult(res, 200))
@@ -44,6 +53,10 @@ class ThingController {
 
   // Creates a new Thing in the DB
   public create(req: express.Request, res: express.Response) {
+    if (!doesUserHavePermission(req.user, 'create:thing')) {
+      this.respondWithResult(res, 401)({message: 'Not allowed to create things!'});
+      return;
+    }
     const user_id = (req.user && req.user.user_id) || null;
     if (!user_id) {
       this.handleError(res, 400);
