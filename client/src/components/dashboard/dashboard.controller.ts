eee44b11eb0fe7a69303eab997b9e5e7a3575758
Ryan Gomoto <ryan@cyrusbio.com>
2017-04-18 18:37:07 -0700

    Create things from dashboard

--- client/src/components/dashboard/dashboard.controller.ts
+++ client/src/components/dashboard/dashboard.controller.ts
@@ -3,12 +3,34 @@ import { AuthService } from '../../auth/auth.service';
 export default class DashboardController {
 
   static $inject = [
-    'AuthService'
+    'AuthService',
+    '$http'
   ];
 
+  public isCreatingThing: boolean;
+
   constructor(
-    private AuthService: AuthService
-  ) {}
+    private AuthService: AuthService,
+    private $http: ng.IHttpService
+  ) {
+    this.isCreatingThing = false;
+  }
+
+  public createThing(name: string) {
+    if (!name) {
+      return;
+    }
+    this.isCreatingThing = true;
+    this.$http({
+      method: 'POST',
+      url: '/api/things',
+      data: { name }
+    })
+    .then((response) => {
+      this.isCreatingThing = false;
+      console.log('Created thing', response);
+    });
+  }
 
   public logout(): void {
     this.AuthService.logout();
