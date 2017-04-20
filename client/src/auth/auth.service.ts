282c0387458a6e743888dd367738866185e61862
Ryan Gomoto <ryan@cyrusbio.com>
2017-04-19 18:00:24 -0700

    Clean up auth service

--- client/src/auth/auth.service.ts
+++ client/src/auth/auth.service.ts
@@ -1,22 +1,9 @@
 import { unauthenticate } from './auth-zero';
 
 class AuthService {
-  public static $inject = [
-    '$q',
-    '$rootScope'
-  ];
-
-  constructor(
-    private $q: ng.IQService,
-    private $rootScope: ng.IRootScopeService
-  ) {}
-
-  // Remember user profile after first fetch.
-  private userProfile: auth0.Auth0UserProfile = null;
-
   // Redirects to auth0.com
   public logout() {
-    unauthenticate()
+    unauthenticate();
   }
 }
 
