54c618b88762b86b44d94d72b31c0afac6df412e
Ryan Gomoto <ryan@cyrusbio.com>
2017-04-27 12:37:52 -0700

    Remove AUTH0_CLIENT_SECRET; not needed because we use RS256 asymmetric signing

--- server/src/settings/auth0.ts
+++ server/src/settings/auth0.ts
@@ -1,7 +1,6 @@
 const settings = {
   domain: process.env.AUTH0_DOMAIN,
   clientId: process.env.AUTH0_CLIENT_ID,
-  clientSecret: process.env.AUTH0_CLIENT_SECRET,
   callbackPath: process.env.AUTH0_CALLBACK_PATH || '/callback',
   silentCallbackPath: process.env.AUTH0_SILENT_CALLBACK_PATH || '/silent-callback'
 };
