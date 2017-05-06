a3e30ff6a4e45d682ef77b0148c5712f04846f27
Ryan Gomoto <ryan@cyrusbio.com>
2017-05-06 16:35:29 -0700

    Move build config to its own file

---
+++ scripts/build.config.js
@@ -0,0 +1,83 @@
+// Static configuration
+
+const root = '.'
+
+module.exports = {
+  client: {
+    scss: {
+      entry: `${root}/client/src/index.scss`,
+      bundle: `${root}/build/client/static/index.css`,
+      options: {
+        minify: true,
+        rev: true,
+        sourcemaps: false
+      }
+    },
+    typescript: {
+      entry: `${root}/client/src/index.ts`,
+      bundle: `${root}/build/client/static/index.js`,
+      options: {
+        minify: true,
+        rev: true,
+        sourcemaps: false,
+        tsconfig: `${root}/client/tsconfig.json`
+      }
+    },
+    vendor: {
+      entry: `${root}/client/package.json`,
+      bundle: `${root}/build/client/static/vendor.js`,
+      options: {
+        minify: true,
+        rev: true,
+        sourcemaps: false
+      }
+    },
+    html: {
+      entry: `${root}/client/src/index.html`,
+      bundle: `${root}/build/client/index.html`,
+      options: {
+        minify: false,
+        inject: {
+          templates: {
+            globs: [`${root}/client/src/*/**/*.html`],
+            cwd: root
+          },
+          globals: {
+            globs: [`${root}/client/src/globals.ts`],
+            cwd: root
+          },
+        }
+      }
+    }
+  },
+  resources: {
+    favicon: {
+      srcGlob: `${root}/resources/images/favicon.ico`,
+      destDir: `${root}/build/resources/images`,
+      options: {
+        rev: true
+      }
+    },
+    images: {
+      srcGlob: `${root}/resources/images/*.png`,
+      destDir: `${root}/build/resources/images`,
+      options: {
+        minify: true,
+        rev: true
+      }
+    }
+  },
+  server: {
+    html: {
+      srcGlob: `${root}/server/src/**/*.html`,
+      destDir: `${root}/build/server`
+    },
+    typescript: {
+      srcGlob: `${root}/server/src/**/!(*.spec).ts`,
+      destDir: `${root}/build/server`,
+      options: {
+        tsconfig: `${root}/server/tsconfig.json`
+      }
+    }
+  }
+}
