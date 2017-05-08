9a93836d5c467164d19cf1dd3b3e97d1de9c9048
Ryan Gomoto <ryan@cyrusbio.com>
2017-05-07 17:29:27 -0700

    Move build task to its own file

---
+++ scripts/tasks/build.js
@@ -0,0 +1,46 @@
+const fs = require('fs')
+const file = require('ido/file')
+const html = require('ido/html')
+const image = require('ido/image')
+const scss = require('ido/scss')
+const typescript = require('ido/typescript')
+const vendor = require('ido/npm')
+
+module.exports = function build(config) {
+  /**
+   * Client
+   */
+  const client = Promise.all([
+    scss.bundle(config.client.scss.entry, config.client.scss.bundle, config.client.scss.options),
+    typescript.bundle(config.client.typescript.entry, config.client.typescript.bundle, config.client.typescript.options),
+    vendor.bundle(config.client.vendor.entry, config.client.vendor.bundle, config.client.vendor.options),
+    image.copy(config.resources.images.srcGlob, config.resources.images.destDir, config.resources.images.options),
+    image.copy(config.resources.favicon.srcGlob, config.resources.favicon.destDir, config.resources.favicon.options)
+  ])
+  .then((manifests) => {
+    const htmlOptions = Object.assign({}, config.client.html.options, { manifests })
+    return html.bundle(config.client.html.entry, config.client.html.bundle, htmlOptions)
+  })
+  .then(() => {
+    console.log('client is done')
+  })
+  .catch((error) => {
+    console.log('client error', error, error.stack)
+  })
+
+  /**
+   * Server
+   */
+  const server = Promise.all([
+    typescript.transpile(config.server.typescript.srcGlob, config.server.typescript.destDir, config.server.typescript.options),
+    file.copy(config.server.html.srcGlob, config.server.html.destDir)
+  ])
+  .then(() => {
+    console.log('server is done')
+  })
+  .catch((error) => {
+    console.log('server error', error, error.stack)
+  })
+
+  return Promise.all([client, server])
+}
