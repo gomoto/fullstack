c23b230ae63bffe7b2c463ec059a73e1272e6b55
Ryan Gomoto <ryan@cyrusbio.com>
2017-05-08 14:19:38 -0700

    Minify each TypeScript module instead of the entire bundle during development

--- scripts/config/dev.build.js
+++ scripts/config/dev.build.js
@@ -3,9 +3,13 @@ const buildConfig = require('./build')
 
 /**
  * Override build configuration.
+ *
  * Setting `rev: false` makes life easier:
  * - html task no longer waits for other tasks that revise file names
  * - html task no longer requires manifests for revised file names.
+ *
+ * Minify each TypeScript module instead of the entire bundle for faster
+ * incremental builds.
  */
 module.exports = deepExtend({}, buildConfig, {
   client: {
@@ -18,11 +22,14 @@ module.exports = deepExtend({}, buildConfig, {
     typescript: {
       options: {
         minify: false,
+        minifyModules: true,
         rev: false
       }
     },
     vendor: {
       options: {
+        minify: false,
+        minifyModules: true,
         rev: false
       }
     }
