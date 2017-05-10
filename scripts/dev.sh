f142eeb6cc0ed64863ae24ce1b70cb8f2a84ee61
Ryan Gomoto <ryan@cyrusbio.com>
2017-05-10 16:51:35 -0700

    Let docker figure out external port for livereload

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -2,7 +2,7 @@
 
 docker pull gomoto/node-docker-compose
 
-# Port 35729 is for LiveReload.
+# Port 35729 is for LiveReload; let docker figure out external port.
 # Pass all environment variables, except TMPDIR.
 docker run \
   -v /var/run/docker.sock:/var/run/docker.sock \
@@ -12,7 +12,7 @@ docker run \
   --tty \
   --rm \
   --expose 35729 \
-  -p 35729:35729 \
+  -p 35729 \
   $(printenv | grep -v ^TMPDIR | sed 's/^/-e /') \
   gomoto/node-docker-compose \
   /bin/bash -c 'ln -s /Users /users; node scripts/dev'
